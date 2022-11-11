bf0a612f95cc8b7f16a872166f7e948a2487272a
Bamidele joshua olayinka <Olajoshua775@gmail.com>
2022-11-11 20:51:01 +0100

    modified

---
+++ index.js
@@ -0,0 +1,12 @@
+function love(){
+        output.innerHTML = '<img src="/img/love.png" alt="" style="height: 43px; width: 40px;">'
+}
+function put(){
+    out.innerHTML = '<img src="/img/love.png" alt="" style="height: 43px; width: 40px;">'
+}
+function pu(){
+    ou.innerHTML = '<img src="/img/love.png" alt="" style="height: 43px; width: 40px;">'
+}
+function pt(){
+    ot.innerHTML = '<img src="/img/love.png" alt="" style="height: 43px; width: 40px;">'
+}
